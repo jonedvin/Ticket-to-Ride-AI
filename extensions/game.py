57dcd3bcaac0636e9e0d45b17a5c1dd839b93fbd
Jon Edvin Lona <jonedvin@Jons-MacBook-Air.local>
2022-12-24 17:08:37 +0100

    Removed run function from Game

--- extensions/game.py
+++ extensions/game.py
@@ -19,30 +19,3 @@ class Game():
         for player in self.players:
             repr += f"\n - {player}"
         return repr
-
-
-    def run(self, last_player = None):
-        """ Main game loop, run recursively. """
-        # Finish the game after the final round
-        if last_player:
-            if self.current_player == last_player:
-                return
-
-        # Stop loop if we reached the final round
-        for player in self.players:
-            if player.train_count <= 2:
-                last_player = self.current_player
-
-
-        # Player turn
-        if type(self.current_player) == Player:
-            pass
-        
-        # AI turn
-        else:
-            pass
-
-
-        # Next turn
-        self.current_player = self.players[self.players.index(self.current_player)+1]
-        self.run(last_player)
